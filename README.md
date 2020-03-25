e2738962bddb4300dd57b9f2f37e1e1daf327836
Gian Ferreira <56806058+gianferreira@users.noreply.github.com>
2020-03-24 23:24:04 -0300

    : Criação das rotas da aplicação
    Criação dos controllers de ongs, incidents, profile e sessions, utilização do knex para integrar com o banco de dados.

---
+++ README.md
@@ -0,0 +1,8 @@
+# omnistack11-backend
+Projeto de API em NodeJS para o backend da Omnistack 11.
+
+Utilizando:
+- express
+- nodemon
+- knex
+- sqlite
--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const app = express();
-
-app.get('/', (req, res) => {
-  res.json({ message: 'Hello World'});
-})
-
-app.listen(3333);
